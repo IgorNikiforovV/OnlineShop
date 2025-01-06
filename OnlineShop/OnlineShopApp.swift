1169d16545a3132c222220688efb8a94612efb2c
Igor Nikiforov <inikiforovj7@icloud.com>
2025-01-06 23:46:09 +0500

    initial commit

---
+++ OnlineShop/OnlineShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnlineShopApp.swift
+//  OnlineShop
+//
+//  Created by Игорь Никифоров on 06.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct OnlineShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
